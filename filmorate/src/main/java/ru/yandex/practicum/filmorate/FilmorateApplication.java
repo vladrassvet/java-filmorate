61f1be20e5101424d13e263f507ea2a546d5c200
vladrassvet <vladrassvet@yandex.ru>
2023-10-05 10:44:50 +0300

    Backup

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,10 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.controllers.FilmController;
-import ru.yandex.practicum.filmorate.models.Film;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FilmorateApplication {
