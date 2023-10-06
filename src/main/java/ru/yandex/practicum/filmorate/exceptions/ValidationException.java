197b11c91b674baa08ddaf52c3686d7bed836f2c
vladrassvet <vladrassvet@yandex.ru>
2023-10-06 12:29:36 +0300

    Backup commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
